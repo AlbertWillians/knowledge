d75189bff93bb9684bf074768467862395c73cfe
Albert Willians <albertwillians@hotmail.com>
2021-06-03 10:09:12 -0300

    estrutura inicial do template

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header />
+		<Menu />
+		<Content />
+		<Footer />
 	</div>
 </template>
 
 <script>
-export default {
-	name: "App",
-}
+	import Header from "./components/template/Header";
+	import Menu from "./components/template/Menu";
+	import Content from "./components/template/Content";
+	import Footer from "./components/template/Footer";
+
+	export default {
+		name: "App",
+		components: { Header, Menu, Content, Footer },
+	};
 </script>
 
 <style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
 
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 40px 1fr 20px;
+		grid-template-columns: 200px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
 </style>
---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+	<footer class="footer">
+		<span>Copyright <strong>Cod3r</strong> © 2015-2018</span>
+	</footer>
+</template>
+
+<script>
+	export default {
+		name: "Footer",
+	};
+</script>
+
+<style>
+	.footer {
+		grid-area: footer;
+		background: #ddd;
+		color: #333;
+
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		padding-right: 10px;
+	}
+</style>
