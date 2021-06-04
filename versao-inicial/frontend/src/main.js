613c7831c375426409056ce303d5af4e585bd366
Albert Willians <albertwillians@hotmail.com>
2021-06-04 12:26:05 -0300

    componente home

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+// TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsYmVydCBXaWxsaWFucyBBcnJlYm9sYSIsImVtYWlsIjoiYWxiZXJ0d2lsbGlhbnNAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjIyODE3NzM1LCJleHAiOjE2MjMwNzY5MzV9.3lIWIT2qI7JWW9fIZf9ZFYh1uTKuh-ReS8HDtTb6RpE'
+
 new Vue({
   store,
   router,
