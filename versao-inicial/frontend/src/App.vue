7393aac80a9ab1c70905dc4eced95061405bd956
Albert Willians <albertwillians@hotmail.com>
2021-06-09 11:42:58 -0300

    Autenticação Validar Token 1

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -12,6 +12,8 @@
 </template>
 
 <script>
+	import axios from "axios";
+	import { baseApiUrl, userKey } from "@/global";
 	import { mapState } from "vuex";
 	import Header from "./components/template/Header";
 	import Menu from "./components/template/Menu";
@@ -22,6 +24,40 @@
 		name: "App",
 		components: { Header, Menu, Content, Footer },
 		computed: mapState(["isMenuVisible", "user"]),
+		data: function() {
+			return {
+				validatingToken: true
+			}
+		},
+		methods: {
+			async validateToken() {
+				this.validatingToken = true
+
+				const json = localStorage.getItem(userKey)
+				const userData = JSON.parse(json)
+				this.$store.commit('setUser', null)
+
+				if(!userData) {
+					this.validatingToken = false
+					this.$router.push({name = 'auth'})
+					return
+				}
+
+				const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+				if(res.data) {
+					this.$store.commit('setUser', userData)
+				} else {
+					localStorage.removeItem(userKey)
+					this.$router.push({name: 'auth'})
+				}
+
+				this.validatingToken = false
+			}
+		},
+		created() {
+			this.validateToken()
+		}
 	};
 </script>
 
